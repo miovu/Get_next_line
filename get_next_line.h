d8c7527c47259d6d63fbfa9315fbba413ab6e0f2
Margarida Iovu <miovu@c1r2s11.42lisboa.com>
2024-11-29 19:14:14 +0000

    get_next_line_bonus.c

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miovu <miovu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 13:22:59 by miovu             #+#    #+#             */
+/*   Updated: 2024/11/28 16:19:53 by miovu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif 
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+int		ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+void	cleanbuff(char *str);
+
+#endif
